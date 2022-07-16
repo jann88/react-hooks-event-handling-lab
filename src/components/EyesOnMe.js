fdb5090a897680e44562d6d7f24add8134ad5e9a
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-07-16 22:43:58 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
+import React from "react";
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    return (
+        <React.Fragment>
+            <button onFocus={() => console.log('Good!')} onBlur={()=> console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </React.Fragment>);
+  }
+  export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
+import React from "react";
 // Code Keypad Component Here
+function Keypad() {
+    return (
+    <React.Fragment>
+         <input onChange={() => console.log("Entering password...")} type="password"/>
+    </React.Fragment>
+    )
+  }
+  export default Keypad;
+  
